fa82bd5e8c853ed85e450d20020fe88eb5153f2d
anjun65 <hajrulkhaira@gmail.com>
2022-12-27 11:49:48 +0700

    test

--- app/Http/Controllers/API/FormLangsungWiringAppController.php
+++ app/Http/Controllers/API/FormLangsungWiringAppController.php
@@ -47,20 +47,17 @@ class FormLangsungWiringAppController extends Controller
         } else {
             $form = FormLangsungWiringApp::create([
                 'forms_id' => $request->form1s_id,
-                'arus_1' => $request->arus_1,
-                'arus_2' => $request->arus_2,
-                'arus_3' => $request->arus_3,
-                'arus_netral' => $request->arus_netral,
-                'voltase_netral_1' => $request->voltase_netral_1,
-                'voltase_netral_2' => $request->voltase_netral_2,
-                'voltase_netral_3' => $request->voltase_netral_3,
-                'voltase_phase_1' => $request->voltase_phase_1,
-                'voltase_phase_2' => $request->voltase_phase_2,
-                'voltase_phase_3' => $request->voltase_phase_3,
-                'cos_1' => $request->cos_1,
-                'cos_2' => $request->cos_2,
-                'cos_3' => $request->cos_3,
-                'akurasi' => $request->akurasi,
+                'terminal1' => $request->terminal1,
+                'terminal2' => $request->terminal2,
+                'terminal3' => $request->terminal3,
+                'terminal4' => $request->terminal4,
+                'terminal5' => $request->terminal5,
+                'terminal6' => $request->terminal6,
+                'terminal7' => $request->terminal7,
+                'terminal8' => $request->terminal8,
+                'terminal9' => $request->terminal9,
+                'terminal11' => $request->terminal11,
+                'keterangan_wiring_app' => $request->keterangan_wiring_app,
             ]);
         }
         
