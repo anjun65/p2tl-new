fc963db0a7a42e378db307905fb07cff476a460a
anjun65 <hajrulkhaira@gmail.com>
2022-12-29 12:38:51 +0700

    rev

--- app/Http/Controllers/API/FormLangsungPemeriksaanPelindungPapanOk.php
+++ app/Http/Controllers/API/FormLangsungPemeriksaanPelindungPapanOk.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use Illuminate\Support\Facades\Storage;
-use App\Models\FormLangsungPemeriksaanPelindungKwh as form_model;
+use App\Models\FormLangsungPemeriksaanPapanOk as form_model;
 
 class FormLangsungPemeriksaanPelindungPapanOk extends Controller
 {
