51c429de689725e6397eb25c7243a4f5bfd04bb2
anjun65 <hajrulkhaira@gmail.com>
2023-01-25 07:15:24 +0700

    rev

--- app/Http/Controllers/Admin/SerahTerimaController.php
+++ app/Http/Controllers/Admin/SerahTerimaController.php
@@ -27,7 +27,6 @@ class SerahTerimaController extends Controller
 
     public function store($id, Request $request)
     {
-        dd($request->all());
         $item = SerahTerima::create([
             'works_id' => $id,
             'tanggal_serah_terima' => $request->tanggal_serah_terima,
