5f4c6b04d3277f49283350f2c62fb91495b7e497
anjun65 <hajrulkhaira@gmail.com>
2023-02-15 10:37:49 +0700

    rev

--- app/Http/Controllers/API/NewFormLangsungController.php
+++ app/Http/Controllers/API/NewFormLangsungController.php
@@ -69,7 +69,8 @@ class NewFormLangsungController extends Controller
             $image = str_replace($replace, '', $image_64);
             $image = str_replace(' ', '+', $image);
             $imageName = Str::random(10) . '.' . $extension;
-            $locate_file_nomor_identitas = Storage::disk('public')->put('assets/saksi/' . $imageName, base64_decode($image));
+            $locate_file_nomor_identitas = Storage::putFile('assets/saksi/' . $imageName, base64_decode($image));
+
             // $locate_file_nomor_identitas = Storage::putFileAs('public/assets/saksi', base64_decode($image), $imageName);
         }
 
