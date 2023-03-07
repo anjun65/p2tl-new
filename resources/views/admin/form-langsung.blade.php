29c0b2eb4e66d331e0fd11c90dbe6bd22392d313
anjun65 <hajrulkhaira@gmail.com>
2023-03-07 16:44:31 +0700

    rev

--- resources/views/admin/form-langsung.blade.php
+++ resources/views/admin/form-langsung.blade.php
@@ -2033,7 +2033,7 @@
                                         </div>
                                       </div>
                               
-                                      @if ($item->work->kalibrasi->file)
+                                      @if ($item->work->kalibrasi->file != null)
                                       <div>
                                         <label class="block text-sm font-medium text-gray-700">Foto Identitas</label>
                                         <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
